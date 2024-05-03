43a447afe95f1e200b4e07a06b505648c26df870
Fabián Huertas <miloh9318@hotmail.com>
2024-05-03 12:34:28 -0500

    primeros pasos creando objeto, funciones y eventos

--- js/app.js
+++ js/app.js
@@ -7,12 +7,33 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
+// UI
 const formulario = document.querySelector('#nueva-cita');
-
 const contenedorCitas = document.querySelector('#citas');
 
 // Eventos
 eventListeners();
 function eventListeners() {
-    
+    mascotaInput.addEventListener('change', datosCita);
+    propietarioInput.addEventListener('change', datosCita);
+    telefonoInput.addEventListener('change', datosCita);
+    fechaInput.addEventListener('change', datosCita);
+    horaInput.addEventListener('change', datosCita);
+    sintomasInput.addEventListener('change', datosCita);
+}
+
+// Objeto de cita
+const citaObj  = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+// Funciones
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);
 }
