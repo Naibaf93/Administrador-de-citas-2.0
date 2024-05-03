aa4c43c8a36a23503cf41ea5ac93c584ca1d69f4
Fabián Huertas <miloh9318@hotmail.com>
2024-05-02 20:59:02 -0500

    agregando primeros selectores

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Selectores
+
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#nueva-cita');
+
+const contenedorCitas = document.querySelector('#citas');
+
+// Eventos
+eventListeners();
+function eventListeners() {
+    
+}
